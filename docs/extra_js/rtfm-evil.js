e0761f129deeeac69ec704a08f00c76b6fa64874
Dan Slinky <26235577+danslinky@users.noreply.github.com>
2024-02-27 04:50:37 +0000

    screensaver mode

--- docs/extra_js/rtfm-evil.js
+++ docs/extra_js/rtfm-evil.js
@@ -1,25 +1,27 @@
+// I <3 uBlock Origin
 let timeoutId;
 
 function resetTimeout() {
     clearTimeout(timeoutId);
-    timeoutId = setTimeout(startScreensaver, 30000);
+    timeoutId = setTimeout(startScreensaver, 15000); // screensaver timeout
 }
 
 function startScreensaver() {
+
     // delete the page
     document.body.innerHTML = "";
 
     // create a screensaver
     const p = document.createElement("p");
-    p.innerHTML = "HIRE @danslinky TO FIX THIS SCREENSAVER";
+    p.innerHTML = "Stuck in vim? HIRE @danslinky for help!";
     document.body.appendChild(p);
 
-    // set the text to white
+    // enable dark mode...
     document.body.style.background = "black";
     document.body.style.color = "white";
     document.body.style.fontSize = "50px";
 
-    // make the text fly around on the page
+    // recreate the dvd logo
     let x = 0;
     let y = 0;
     let dx = 1;
@@ -39,16 +41,48 @@ function startScreensaver() {
         }
     }, 10);
 
+    // change the tab <title>
+    document.title = "pornhub.com";
+
     // reload the page when the mouse is moved
-    document.onmousemove = () => {
-        location.reload();
+    // document.onmousemove = () => {
+    //     location.reload();
+    // }
+    // vim escape mode activated
+    document.onkeydown = (e) => {
+        if (e.key === "Escape") {
+            document.onkeydown = (e) => {
+                if (e.key === ":") {
+                    document.onkeydown = (e) => {
+                        if (e.key === "q") {
+                            location.reload();
+                        }
+                    }
+                }
+            }
+        }
     }
+
+    // told you, disable javascript
+    
 }
 
+// reset the timeout on any user interaction
 document.addEventListener('mousemove', resetTimeout);
 document.addEventListener('mousedown', resetTimeout);
 document.addEventListener('keydown', resetTimeout);
 document.addEventListener('scroll', resetTimeout);
 document.addEventListener('touchstart', resetTimeout);
+document.addEventListener('click', resetTimeout);
+document.addEventListener('keypress', resetTimeout);
+document.addEventListener('keyup', resetTimeout);
+document.addEventListener('keydown', resetTimeout);
+document.addEventListener('resize', resetTimeout);
+document.addEventListener('wheel', resetTimeout);
+document.addEventListener('touchmove', resetTimeout);
+document.addEventListener('touchend', resetTimeout);
+document.addEventListener('touchcancel', resetTimeout);
+document.addEventListener('touchleave', resetTimeout);
+document.addEventListener('touchenter', resetTimeout);
 
 resetTimeout(); // start the initial timeout
